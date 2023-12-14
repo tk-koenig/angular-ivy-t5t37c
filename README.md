2f414b7fb8728936b4b9ec0449a164e525efa7b3
tk-koenig <74401998+tk-koenig@users.noreply.github.com>
2023-12-14 15:17:13 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-t5t37c
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/tk-koenig/angular-ivy-t5t37c)
---
+++ src/app/Components/vse-settings-card.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hri-vse-settings-card',
+  templateUrl: './vse-settings-card.component.html',
+  styleUrls: ['./vse-settings-card.component.css'],
+})
+export class VseSettingsCardComponent {}
